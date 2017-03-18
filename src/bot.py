42a764800f99433988a4607fcf4b867c7c2c1884
Christen Ford <christen0ford@gmail.com>
2017-03-18 17:38:04 -0400

    Remove Source Fiels

--- src/bot.py
+++
@@ -1,8 +0,0 @@
-from Player import Player
-
-class Bot(player):
-    def __str__(self):
-        return "Bots Chips: {0}".format(self.chips);
-    
-    def determineAction(self):
-        Util.raiseNotDefined();
