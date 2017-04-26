ca427d1a2dedf76462fdb1b14dc9c265ba10849b
Christen Ford <cford15@mail.bw.edu>
2017-04-26 15:51:18 -0400

    Update player.py

--- src/player.py
+++ src/player.py
@@ -131,7 +131,7 @@ class Player:
     '''
     def getBet(self, raiseAmt):
         amt = -1;
-        while amt < raiseAmt or amt > self.player.getChips():
+        while amt < raiseAmt or amt > self.getChips():
             try:
                 amt = int(raw_input("Raise By: "));
             except ValueError:
